3de964a8783923dac9109a75d904f6e905953f9d
suyeonnii <suyeon9288@gmail.com>
2025-09-12 04:45:08 +0900

    chore: 배포

--- config/settings.py
+++ config/settings.py
@@ -150,7 +150,8 @@ SWAGGER_SETTINGS = {
             'name': 'Authorization',
             'in': 'header'
         }
-    }
+    },
+    "DEFAULT_API_URL": "https://rumz.site",
 }
 
 # Password validation
@@ -188,7 +189,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
