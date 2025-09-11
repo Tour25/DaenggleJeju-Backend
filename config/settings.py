73bfdcd59e6635c34a6bcebc5442860088144965
suyeonnii <suyeon9288@gmail.com>
2025-09-12 03:53:11 +0900

    chore: 배포

--- config/settings.py
+++ config/settings.py
@@ -183,7 +183,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = '/staticfiles'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = '/media'
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
